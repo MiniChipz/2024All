91c2c7b84917648087437f2a4eaa3ca2ae39ae25
MiniChipz <minidip347@gmail.com>
2024-09-12 12:50:59 +0200

    rustCommit

---
+++ Rust/firstTest/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+
+    let answer: u8 = rand::thread_rng().gen_range(1..=10);
+
+    println!("Guess a number");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Error!");
+
+    match guess.trim().parse::<u8>() { 
+        
+    }
+} 
